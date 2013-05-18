51945000b21ddbf3de7d947eeb3d80a1f127ebbf
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-05-18 12:22:45 +0100

    bump clojure version

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject enclog "0.5.8-SNAPSHOT"
+(defproject enclog "0.5.8"
   :description "Thin Clojure wrapper for Encog(v3) Machine-Learning framework."
   :url "http://github.com/jimpil/enclog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.encog/encog-core "3.1.0"]
                  ]
   :jvm-opts ["-Xmx1g"] 
   ;:javac-options {:classpath "target/dependency/encog-core-3.1.0.jar" :destdir "target/classes"}
-  ;:java-source-path "src/java"
-  :main     enclog.examples
+  ;:java-source-paths ["src/encog_java"]
+  ;:main     enclog.examples
   )
