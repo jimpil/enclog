48d203f257760ae3e41b45626a70a9ad6ec315a9
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-05-28 20:21:32 +0100

    converting to compliant defproject for signing

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
-(defproject enclog "0.6.0"
+(defproject enclog "0.6.1"
   :description "Thin Clojure wrapper for Encog(v3) Machine-Learning framework."
   :url "http://github.com/jimpil/enclog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.encog/encog-core "3.1.0"]]
-  :jvm-opts ["-Xmx1g"] 
+  :jvm-opts ["-Xmx1g"]
+  :scm {:name "git"
+         :url "https://github.com/jimpil/enclog"} 
   ;:java-source-paths ["src/encog_java"]
   ;:main     enclog.examples
   )
