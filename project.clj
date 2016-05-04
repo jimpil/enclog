6ca0cb3e203d50d6b30104a68f84974076de3d5e
Dimitrios Piliouras <jimpil1985@gmail.com>
2016-05-04 19:48:05 +0100

    mark release 0.6.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject enclog "0.6.5"
+(defproject enclog "0.6.6"
   :description "Thin Clojure wrapper for Encog(v3) Machine-Learning framework."
   :url "http://github.com/jimpil/enclog"
   :license {:name "Eclipse Public License"
